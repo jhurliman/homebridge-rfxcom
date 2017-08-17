313d41118bec63eb1603fa197df37572ea1d6ddb
John Hurlman <jhurliman@jhurliman.org>
2017-08-17 08:18:42 -0700

    1.0.0

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const rfxcom = require('rfxcom')
 
 const PLUGIN_ID = 'homebridge-rfxcom'
 const PLUGIN_NAME = 'RFXCom'
+const DEFAULT_OPEN_CLOSE_SECONDS = 5
 
 let Accessory, Service, Characteristic, UUIDGen
 
@@ -39,7 +40,14 @@ function RFXComPlatform(log, config, api) {
 
 // Method to restore accessories from cache
 RFXComPlatform.prototype.configureAccessory = function(accessory) {
-  this.setService(accessory)
+  this.log(
+    `Loaded from cache: ${accessory.context.name} (${accessory.context
+      .switchID})`
+  )
+
+  const existing = this.accessories[accessory.context.switchID]
+  if (existing) this.removeAccessory(existing)
+
   this.accessories[accessory.context.switchID] = accessory
 }
 
@@ -48,27 +56,31 @@ RFXComPlatform.prototype.didFinishLaunching = function() {
   // Add or update accessory in HomeKit
   if (this.rfyRemotes.length) {
     // Compare local config against RFXCom-registered remotes
-    this.listRFYRemotes().then(deviceRemotes => {
-      this.log(`Received ${deviceRemotes.length} remote(s) from device`)
+    this.listRFYRemotes()
+      .then(deviceRemotes => {
+        this.log(`Received ${deviceRemotes.length} remote(s) from device`)
 
-      this.rfyRemotes.forEach(remote => {
-        // Handle different capitalizations of deviceID
-        remote.deviceID = remote.deviceID || remote.deviceId
+        this.rfyRemotes.forEach(remote => {
+          // Handle different capitalizations of deviceID
+          remote.deviceID = remote.deviceID || remote.deviceId
 
-        const deviceID = remote.deviceID
-        const device = deviceRemotes.find(dR => deviceID === dR.deviceId)
+          const deviceID = remote.deviceID
+          const device = deviceRemotes.find(dR => deviceID === dR.deviceId)
 
-        if (device) {
-          // Remote found on the RFXCom device
-          this.addRFYRemote(remote, device)
-          this.log(`Added accessories for RFY remote ${remote.deviceID}`)
-        } else {
-          // No remote found on device
-          const msg = deviceRemotes.map(dR => `${dR.deviceId}`).join(', ')
-          this.log(`ERROR: RFY remote ${deviceID} not found. Found: ${msg}`)
-        }
+          if (device) {
+            // Remote found on the RFXCom device
+            this.addRFYRemote(remote, device)
+            this.log(`Added accessories for RFY remote ${remote.deviceID}`)
+          } else {
+            // No remote found on device
+            const msg = deviceRemotes.map(dR => `${dR.deviceId}`).join(', ')
+            this.log(`ERROR: RFY remote ${deviceID} not found. Found: ${msg}`)
+          }
+        })
       })
-    })
+      .catch(err => {
+        this.log(`UNHANDLED ERROR: ${err}`)
+      })
   } else {
     // FIXME: Setup mode
     this.log(`WARN: No RFY remotes configured`)
@@ -81,8 +93,8 @@ RFXComPlatform.prototype.listRFYRemotes = function() {
     this.rfxtrx.once('rfyremoteslist', remotes => resolve(remotes))
 
     this.rfxtrx.initialise(() => {
-      this.log('RFXtrx initialized')
-      rfy.listRemotes()
+      this.log('RFXtrx initialized, listing remotes...')
+      this.rfy.listRemotes()
     })
   })
 }
@@ -105,9 +117,9 @@ RFXComPlatform.prototype.addRFYRemoteSwitch = function(remote, device, type) {
   // Setup accessory
   let accessory = this.accessories[switchID]
   if (accessory) this.removeAccessory(accessory)
-  
+
   const name = `${remote.name} ${type}`
-  const uuid = UUIDGen.generate(seed)
+  const uuid = UUIDGen.generate(switchID)
   accessory = new Accessory(remote.name, uuid)
 
   this.accessories[switchID] = accessory
@@ -141,18 +153,23 @@ RFXComPlatform.prototype.addRFYRemoteSwitch = function(remote, device, type) {
 
   // Setup event listeners
   accessory
-    .on('identify', this.identify.bind(this, accessory))
+    .on('identify', callback => {
+      this.log(`${name} identify requested!`)
+      callback()
+    })
     .getService(Service.Switch)
     .getCharacteristic(Characteristic.On)
     .on('get', callback => callback(null, accessory.context.isOn))
     .on('set', (value, callback) => {
-      // Issue a stop if any switch is toggled off
-      if (!value) {
+      // Issue a stop if any switch is toggled off or the Stop switch is hit
+      if (!value || type === 'Stop') {
         console.log(`RFY STOP ${remote.deviceID}`)
         this.rfy.stop(remote.deviceID)
 
-        for (const t in remote.switches)
-          this.setSwitch(remote.switches[t], false)
+        setTimeout(() => {
+          for (const t in remote.switches)
+            this.setSwitch(remote.switches[t], false)
+        }, 100)
 
         return callback()
       }
@@ -166,19 +183,16 @@ RFXComPlatform.prototype.addRFYRemoteSwitch = function(remote, device, type) {
           console.log(`RFY DOWN ${remote.deviceID}`)
           this.rfy.down(remote.deviceID)
           break
-        case 'Stop':
-          console.log(`RFY STOP ${remote.deviceID}`)
-          this.rfy.stop(remote.deviceID)
-          break
       }
 
-      // Show the switch as on
-      this.setSwitch(accessory, true)
+      // Toggle all switches to the correct on/off state
+      for (const t in remote.switches)
+        this.setSwitch(remote.switches[t], t === type)
 
       // After a configurable amount of time, toggle the switch back to off
       const ms = isNaN(remote.openCloseSeconds)
-        ? 1000 * 10
-        : remote.openCloseSeconds * 1000
+        ? DEFAULT_OPEN_CLOSE_SECONDS * 1000
+        : Math.round(remote.openCloseSeconds * 1000)
       clearTimeout(accessory.timerID)
       accessory.timerID = setTimeout(() => this.setSwitch(accessory, false), ms)
 
@@ -195,12 +209,11 @@ RFXComPlatform.prototype.addRFYRemoteSwitch = function(remote, device, type) {
 }
 
 RFXComPlatform.prototype.setSwitch = function(accessory, isOn) {
-  this.log(`Setting switch ${accessory.context.switchID} to ${isOn}`)
+  this.log(`Updating switch ${accessory.context.switchID}, on=${isOn}`)
 
   accessory.context.isOn = isOn
   accessory
     .getService(Service.Switch)
-    .setCharacteristic(Characteristic.On, isOn)
     .getCharacteristic(Characteristic.On)
     .getValue()
 }
@@ -218,10 +231,4 @@ RFXComPlatform.prototype.removeAccessory = function(accessory) {
 // Method to remove all accessories from HomeKit
 RFXComPlatform.prototype.removeAccessories = function() {
   this.accessories.forEach(id => this.removeAccessory(this.accessories[id]))
-}
-
-// Method to handle identify request
-RFXComPlatform.prototype.identify = function(acc, paired, callback) {
-  this.log(`${acc.name} identify requested!`)
-  callback()
 }
