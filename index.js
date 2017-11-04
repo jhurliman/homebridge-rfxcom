29bb7e4a6699763fc4786001d7da45ab10b6838e
John Hurliman <jhurliman@jhurliman.org>
2017-11-03 19:40:52 -0700

    Fix crash bug: access of this.config before definition

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ module.exports = function(homebridge) {
 }
 
 function RFXComPlatform(log, config, api) {
-  const rfyRemotes = this.config.rfyRemotes || this.config.rfyremotes
-
   this.log = log
   this.config = config || { platform: 'RFXCom' }
   this.tty = this.config.tty || '/dev/ttyUSB0'
   this.debug = this.config.debug || false
+
+  const rfyRemotes = this.config.rfyRemotes || this.config.rfyremotes
   this.rfyRemotes = Array.isArray(rfyRemotes) ? rfyRemotes : []
 
   this.accessories = {}
